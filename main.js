a326af246e44d3d1b01d9ce47ac00d6aa23c4a3f
André Cirne <ancirne@gmail.com>
2017-10-12 22:21:19 +0100

    Retabuação e indexação  e inciio do formulário de configurações

--- main.js
+++ main.js
@@ -3,8 +3,23 @@ function login() {
     document.getElementById('registo').style.display = "none";
     document.getElementById('login_form').style.display = "none";
     document.getElementById('barra_lateral').style.display = "inline";
-
-
-
     document.getElementById('login_').innerHTML += ('Welcome, '+user+"!");
+}
+
+function close_panels(){
+    var elements = document.getElementsByClassName('panel')
+    for (var i = 0; i < elements.length ; ++i) {
+        elements[i].style.display = "none";
+    }
+}
+
+
+function open_config(){
+    close_panels();
+
+    var painel = document.getElementById('configuracao');
+    // Inicialização do form
+
+    painel.style.display = "inline";
+
 }
