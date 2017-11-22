fc96e944b9819f5afbf6c7a9b27c55f15ecfb95e
Unknown <zepedro9753@gmail.com>
2017-11-22 12:32:26 +0000

    Login está a funcionar.

--- requests.js
+++ requests.js
@@ -2,32 +2,37 @@ var serverApi = "http://twserver.alunos.dcc.fc.up.pt:8008";
 
 function registerApi(login, password) {
   content ={
-    "name":login,
+    "nick":login,
     "pass":password,
   };
 
   post_content = JSON.stringify(content);
 
-   if(!XMLHttpRequest) {
-     console.log("XHR não é suportado");
-     return;
-   }
+  if(!XMLHttpRequest) {
+    console.log("XHR não é suportado");
+    return;
+  }
 
-   var xhr = new XMLHttpRequest();
-   xhr.onreadystatechange = function() {
-        if(xhr.readyState== 4 && xhr.status == 200){
-          console.log("entre");
-            res  = JSON.parse(xhr.responseText);
-            if(isEmpty(res)){
-              changeToLoged(login);
-            }
-            else{
-              console.log(res["error"]);
-              alert(res["error"]);
-            }
-        }
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST",serverApi+"/register", true);
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState < 4){
+      return;
     }
+    res  = JSON.parse(xhr.responseText);
+    if(xhr.status == 200){
+      if(isEmpty(res)){
+        changeToLoged(login);
+      }
+      else{
+        console.log(res["error"]);
+        alert(res["error"]);
+      }
+    }
+    else{
+      alert(res["error"]);
+    }
+  }
 
-   xhr.open("POST",serverApi+"/register", true);
-   xhr.send(post_content);
+  xhr.send(post_content);
 }
